113292d461a66825779075885ac70ea4cdf129d1
Vu Quoc Ngoc <vuquocngoc1989@gmail.com>
2022-12-31 21:24:32 +0700

    update01

---
+++ 01_intro.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+1.Class & Object
+ * 2.Property & Method
+ * 3.Constructor & Destructor
+ * 4.Extends & Override
+ * 5.Final
+ * 6.Public - Private - Protected
+ * 7.Static
+ * 8.Parent & Self
+ * 9.constant
+ * 10.clone()
+ * 11.__autoload()
+ * 12.spl_autoload_register()
+ * 13.__sleep() & __wakeup()
+ * 14.toString()
+ * 15.__set() & __get
+ */
+
